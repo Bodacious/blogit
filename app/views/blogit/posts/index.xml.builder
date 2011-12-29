709667650b7cfd720db27540af93f8a635d96cc8
Gavin Morrice <gavin@gavinmorrice.com>
2011-12-29 10:22:07 +0000

    Fixed sitemap routing and index xml structure

--- app/views/blogit/posts/index.xml.builder
+++ app/views/blogit/posts/index.xml.builder
@@ -1,10 +1,10 @@
 xml.instruct! 
 xml.urlset :xmlns => 'http://www.sitemaps.org/schemas/sitemap/0.9' do 
 
-  for post in posts
+  for post in @posts
     xml.url do
-      xml.loc post_url(story)
-      xml.lastmod story.updated_at.xmlschema
+      xml.loc post_url(post)
+      xml.lastmod post.updated_at.xmlschema
     end
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Blogit::Engine.routes.draw do
   resources :posts, except: [:index] do
     resources :comments, only: [:create, :destroy]
   end
-  
+
+  match "sitemap.xml" => 'posts#index', format: :xml
+
   match "page/:page" => "posts#index"
+
   match "posts/tagged/:tag" => 'posts#tagged', as: :tagged_blog_posts
   
   root to: "posts#index"
