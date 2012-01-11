48d30daa49b677fdb91d534bbaf894f103c5083c
Gavin Morrice <gavin@gavinmorrice.com>
2012-01-11 09:35:12 +0000

    Reorganized routes to fix pagination with caching

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Blogit::Engine.routes.draw do
   
+  # Keep these above the posts resources block
+  match "posts/page/:page" => "posts#index"
+  match "posts/tagged/:tag" => 'posts#tagged', as: :tagged_blog_posts
+    
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
 
-  match "page/:page" => "posts#index"
-
-  match "posts/tagged/:tag" => 'posts#tagged', as: :tagged_blog_posts
-  
   root to: "posts#index"
 end
