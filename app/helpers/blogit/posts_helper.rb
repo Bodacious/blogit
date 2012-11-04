4498a6e9dd9a206d297fbffdc3747f319e391208
Gavin Laking <gavinlaking@gmail.com>
2012-11-04 16:29:52 +0000

    Refactored helpers CommentsHelper#blog_comment_tag and PostsHelper#blog_post_tag into ApplicationHelper#blog_tag

--- app/helpers/blogit/posts_helper.rb
+++ app/helpers/blogit/posts_helper.rb
@@ -1,20 +1,6 @@
 module Blogit
   module PostsHelper
 
-    # Creates a div tag with class 'blog_post_' + name
-    # Eg:
-    #   blog_post_tag(:title, "") # => <div class="blog_post_title"></div>
-    def blog_post_tag(name, content_or_options = {}, options ={}, &block)
-      if block_given?
-        content = capture(&block)
-        options = content_or_options
-      else
-        content = content_or_options
-      end
-      options[:class] = "#{options[:class]} blog_post_#{name}".strip
-      content_tag(name, content, options)
-    end
-
     # A comments tag corresponding to the comments configuration
     def comments_for(post)
       render(partial: "blogit/posts/#{Blogit.configuration.include_comments}_comments", locals: { post: post, comment: Blogit::Comment.new })
