0cfb487f9371e9ad732e31ec74160392f029bba5
Bodacious <gavin@katanacode.com>
2013-04-14 18:57:46 +0100

    Added blogit view generators
    Closes #16

---
+++ lib/generators/blogit/views_generator.rb
@@ -0,0 +1,19 @@
+module Blogit
+  module Generators
+
+    class ViewsGenerator < Rails::Generators::Base
+      desc "Copies Blogit views to your application."
+
+      include Thor::Actions
+
+      source_root File.expand_path('../../../../app/views', __FILE__)
+
+      # Copy all of the views from the blogit/app/views/blogit to
+      # app/views/blogit
+      def copy_views
+        directory 'blogit', 'app/views/blogit'
+      end
+
+    end
+  end
+end
