61d2e602b6df833312897e44d38e6bb37a745e4d
Philippe Durix <philippe.durix@me.com>
2013-06-17 16:49:26 +0200

    Added an archive feature and created archive/tag helpers

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Blogit::Engine.routes.draw do
   # Keep these above the posts resources block
   match "posts/page/:page" => "posts#index"
   match "posts/tagged/:tag" => 'posts#tagged', as: :tagged_blog_posts
+  match "posts/archive/:year/:month" => 'posts#archive', as: :archive_blog_posts
 
   resources :posts do
     resources :comments, only: [:create, :destroy]
