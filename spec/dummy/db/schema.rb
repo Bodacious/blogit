3d38b8187b7ae53d8f3d5ccb222fa44e3d7c3b67
Bodacious <gavin@katanacode.com>
2013-08-19 23:21:40 +0100

    rake db:migrate updated schema.rb file

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130819160002) do
+ActiveRecord::Schema.define(:version => 20130819160229) do
 
   create_table "blogit_comments", :force => true do |t|
     t.string   "name",       :null => false
