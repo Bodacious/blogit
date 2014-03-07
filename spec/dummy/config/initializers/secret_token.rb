d85b35558722efb767953d49b70df9b8a7e24c92
Bodacious <gavin@katanacode.com>
2014-03-07 16:45:43 +0000

    Updated secret token and application.rb to satisfy deprecation warnings

--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'b4bfcb0cc0064008add1ac3db2123467882dfdfbdd5e66487bf83c671792a03a18c7795ef8371a9ccf18d19f497b08d2d2f7e81fb993cf43f3443ae1838bab25'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'f72bc586e221e4d19f9746f9a4ca38a75cce2deaabf584fb378e5f30e0c226e14dbb8245befed1060092c74d2e6804a50a531c351f994b7a10edf2bdbd6fbefd'
