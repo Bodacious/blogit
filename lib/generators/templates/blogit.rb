5a16b5d75fd154e7873fb5aa4a2a9fa79ec3402d
Bodacious <gavin@katanacode.com>
2015-06-11 12:07:50 +0100

    Added setup rake tasks to travis.yml

--- lib/generators/templates/blogit.rb
+++ lib/generators/templates/blogit.rb
@@ -13,11 +13,6 @@ Blogit.configure do |config|
   # Twitter username used in the share bar
   config.twitter_username = ""
 
-  # The name of the controller method we'll call to return the current blogger.
-  # Change this if you use something other than current_user.
-  # Eg. current_admin_user (if using ActiveAdmin)
-  config.current_blogger_method = :current_user
-
   # What method do we call on blogger to show who they are?
   config.blogger_display_name_method = :username
 
@@ -27,22 +22,9 @@ Blogit.configure do |config|
   # No. of posts to show per page
   config.posts_per_page = 5
 
-  # The name of the before filter we'll call to authenticate the current user.
-  config.authentication_method = :login_required
-
   # If set to true, the comments form will POST and DELETE to the comments
   # controller using AJAX calls.
   config.ajax_comments = true
-
-  # If set to true, the create, edit, update and destroy actions
-  # will be included. If set to false, you'll have to set these
-  # yourself elsewhere in the app.
-  config.include_admin_actions = true
-
-  # If set to true, links for new posts, editing posts and deleting comments
-  # will be available. If set to false, you'll have to set these
-  # yourself in the templates.
-  config.include_admin_links = true
 
   # The default format for parsing the blog content.
   config.default_parser = :markdown
@@ -56,15 +38,6 @@ Blogit.configure do |config|
 
   # RSS Feed description content
   config.rss_feed_description = "Latest from #{Rails.application.engine_name.titleize}"
-
-  # Should blogit ping search engines with your sitemap
-  # when posts are created, updated or destroyed?
-  #
-  # Can pass true to ping all supported search engines, or an array of search
-  # engine names. e.g. [:google, :bing]
-  #
-  # Defaults to false
-  config.ping_search_engines = false
 
   # When using redcarpet as content parser, pass these options as defaults.
   # @see here for more options: https://github.com/tanoku/redcarpet
