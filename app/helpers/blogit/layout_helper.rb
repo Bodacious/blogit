8acd800854eb7accefa02908252b748168a190d7
Bodacious <gavin@katanacode.com>
2015-06-19 11:19:01 +0100

    Added partial for social media meta tags

--- app/helpers/blogit/layout_helper.rb
+++ app/helpers/blogit/layout_helper.rb
@@ -1,11 +1,19 @@
 module Blogit
   
   module LayoutHelper
+
+    def title(content=nil)
+      if content
+        content_for(:title, content)
+      else
+        content_for(:title)
+      end
+    end
     
     
     def description(content=nil)
       if content
-        content_for(:description, content)
+        content_for(:description, strip_tags(format_content(content)).html_safe)
       else
         content_for(:description)
       end
