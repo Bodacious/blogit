b779b05bc64cd89ea1f6b98e8f27e1cae6808771
Karsten Silkenbäumer <ksi@kluks.de>
2015-11-24 00:50:50 +0100

    Fix comments count not being shown due to several mistakes

--- app/helpers/blogit/posts_helper.rb
+++ app/helpers/blogit/posts_helper.rb
@@ -1,13 +1,13 @@
 module Blogit
   module PostsHelper
-    
+
     require "blogit/archive"
-    
+
     # Renders the comments for a {Post} based on the
     # {Blogit::Configuration::include_comments} configuration
     def comments_for_post(post)
       comment_type = Blogit.configuration.include_comments
-      render(partial: "blogit/comments/#{comment_type}_comments", 
+      render(partial: "blogit/comments/#{comment_type}_comments",
         locals: { post: post, comment: Blogit::Comment.new })
     end
 
@@ -17,7 +17,7 @@ module Blogit
       return "" unless Blogit.configuration.include_share_bar
       render(partial: "blogit/posts/share_bar", locals: { post: post})
     end
-    
+
     # Returns the {Post Posts} that share one or more of the same tags for a given post
     #
     # post - A {Post} instance
@@ -25,6 +25,10 @@ module Blogit
     # Returns a collection of {Post Posts}
     def related_posts_for_post(post)
       post.find_related_on_tags.active
+    end
+
+    def show_comments_count?
+      Blogit.configuration.include_comments == :active_record
     end
 
     # Creates a ul tag tree with posts by year and months. Include blogit/archive.js in
@@ -38,7 +42,7 @@ module Blogit
     #            ...
     #          </ul>
     #        </div>
-    # 
+    #
     # Returns an HTML safe String
     def archive_list_for_posts(archive_posts)
       render Blogit::Archive::List.new(archive_posts)
