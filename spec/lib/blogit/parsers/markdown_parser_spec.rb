78e238792b8957870276db4ca691632bb21f8936
Bodacious <gavin@gavinmorrice.com>
2016-03-12 00:43:44 +0000

    Fixed broken test with new pygmentize

--- spec/lib/blogit/parsers/markdown_parser_spec.rb
+++ spec/lib/blogit/parsers/markdown_parser_spec.rb
@@ -16,47 +16,48 @@ describe Blogit::Parsers::MarkdownParser do
     }
 
     it "requires pymentize to run" do
-      expect(system("pygmentize > /dev/null")).to equal(true), "It seems that pygmentize is not installed on your system"
+      expect(system("which pygmentize")).not_to eql(false),
+        "It seems that pygmentize is not installed on your system"
     end
 
     context "when highlight_code_syntax is true" do
-      
+
       before do
         Blogit::configuration.highlight_code_syntax = true
       end
-      
+
       it "should raise an exception if pygments isn't installed" do
         original_path = ENV['PATH']
         ENV['PATH']   = ""
         expect { parser.parsed }.to raise_error(RuntimeError)
         ENV["PATH"] = original_path
       end
-      
+
       it "should highlight code syntax" do
         expect(parser.parsed).to match(
         Regexp.new("<h2>Header</h2>\n<div class=\"highlight\"><pre><span class=\"nb\">puts</span> <span class=\"s1\">&#39;hello world&#39;</span>\n</pre>\n</div>\n")
         )
       end
-      
+
     end
 
     context "when highlight_code_syntax is false" do
-      
+
       before do
         Blogit::configuration.highlight_code_syntax = false
       end
-      
+
       it "should not raise an exception, even if pygments isn't installed" do
         original_path = ENV['PATH']
         ENV['PATH']   = ""
         expect { parser.parsed }.to_not raise_error
         ENV["PATH"] = original_path
       end
-      
+
       it "shoud not highlight code" do
         expect(parser.parsed).to eq("<h2>Header</h2>\n\n<pre><code class=\"ruby\">puts &#39;hello world&#39;\n</code></pre>\n")
       end
-      
+
     end
 
   end
